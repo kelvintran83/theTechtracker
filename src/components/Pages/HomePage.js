c62ad002ee198a9042ac12e5f661b4ab007cbed1
s3781137 <s3781137@student.rmit.edu,au>
2023-09-05 13:14:38 +1000

    Save article functionality working, firestore db implemented. Utilising two collections for articles belonging to userID and hashing url, as well as a collection for storing the true URL of hashed URL.`

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import {debounce} from 'lodash';
 import './HomePage.css';
+import StarIcon from '../../assets/star.svg'
+import EmptyStarIcon from '../../assets/star-empty.svg'
+import {doc, setDoc, deleteDoc, getDoc, getDocs,  collection, query, where} from 'firebase/firestore'
+import {db} from '../../firebase'
+import {useAuth} from '../../contexts/AuthContexts'
+
 
 export default function HomePage({articles, formatDescription }) {
-  const apiKey = '2a8a8b489e2e43b0af552d15e840cc38';
-  const pageSize = 5;
+  const apiKey = 'ff014538be754c0b84af12098f3a6483'
+  const pageSize = 5
 
-  const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
+  const [newsData, setNewsData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [savedArticles, setSavedArticles] = useState([])
+
+  const { currentUser } = useAuth();
 
   const companyNames = [
     'Google',
@@ -20,41 +29,85 @@ export default function HomePage({articles, formatDescription }) {
     'Apple',
   ];
 
-  const fetchArticles = async () => {
+ const fetchArticles = async () => {
     try {
       const responses = await Promise.all(
-        companyNames.map(companyName =>
-          fetch(
+        companyNames.map(async (companyName) => {
+          const response = await fetch(
             `https://newsapi.org/v2/everything?q=${companyName}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`
-          ).then(response => response.json())
-        )
+          );
+          const data = await response.json();
+          return data.articles;
+        })
       );
 
-      const newArticles = responses.flatMap(response => response.articles);
+      const newArticles = responses.flatMap(articles => articles);
       const realArticles = newArticles.filter(article => (
         article.url && article.title && article.author && article.urlToImage && article.publishedAt && article.content
       ));
       const uniqueArticles = realArticles.filter(
         (article, index, self) => index === self.findIndex(a => a.url === article.url)
       );
+
       setNewsData(prevNewsData => [...prevNewsData, ...uniqueArticles]);
-      setLoading(false);
+      setLoading(false)
     } catch (error) {
-      console.error("Error fetching data:", error);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchArticles();
-  }, [page]);
-
-  function handleScroll() {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 && loading){
-      setPage(prevPage => prevPage + 1);
-      setLoading(true);
+      console.error("Error fetching data:", error)
+      setLoading(false)
     }
   }
+
+  useEffect(() => {
+    fetchArticles()
+  }, [page]);
+
+
+  function handleScroll() {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 && loading) {
+      setPage(prevPage => prevPage + 1)
+      setLoading(true)
+    }
+  }
+
+  const loadSavedArticles = async () => {
+    try {
+      if (currentUser) {
+        const userSavedArticlesCollectionRef = collection(db, 'savedArticles');
+        const userSavedArticlesQuery = query(
+          userSavedArticlesCollectionRef,
+          where('user', '==', currentUser.uid) 
+        );
+
+        const querySnapshot = await getDocs(userSavedArticlesQuery);
+        const savedArticlesData = [];
+
+        querySnapshot.forEach(async (docu) => {
+          const hashedURL = docu.id;
+          const originalURLRef = doc(db, 'hashedToOriginalURLs', hashedURL);
+          const originalURLDoc = await getDoc(originalURLRef);
+
+          if (originalURLDoc.exists()) {
+            savedArticlesData.push({
+              hashedURL,
+              originalURL: originalURLDoc.data().originalURL,
+            })
+          } else {
+            console.error(`Original URL not found for hashed URL: ${hashedURL}`);
+          }
+        })
+        console.log(savedArticlesData)
+        setSavedArticles(savedArticlesData);
+      }
+    } catch (error) {
+      console.error('Error loading saved articles:', error);
+    }
+  }
+
+  useEffect(() => {
+    loadSavedArticles();
+    
+  }, [currentUser]);
+
 
   const debouncedHandleScroll = debounce(handleScroll, 250);
 
@@ -66,24 +119,90 @@ export default function HomePage({articles, formatDescription }) {
     };
   }, [page]);
 
+
+  function generateIdFromUrl(url) {
+    const sanitisedUrl = url.replace(/[/\\.#$]/g, '_')
+    return hashCode(sanitisedUrl)
+  }
+
+  function hashCode(str) {
+    let hash = 0
+    for (let i = 0; i < str.length; i++) {
+      const char = str.charCodeAt(i)
+      hash = (hash << 5) - hash + char
+    }
+    return String(hash)
+  }
+
+  async function toggleSaveArticles(article) {
+    if (!currentUser) {
+      return
+    }
+
+    try {
+      const articleId = generateIdFromUrl(article.url);
+      const articleRef = doc(db, 'savedArticles', articleId);
+      const docSnapshot = await getDoc(articleRef);
+
+      if (docSnapshot.exists()) {
+        await deleteDoc(articleRef);
+        setSavedArticles((prevSavedArticles) =>
+          prevSavedArticles.filter(
+            (savedArticle) => savedArticle.hashedURL !== articleId
+          )
+        )
+      } else {
+        await setDoc(articleRef, {
+          user: currentUser.uid, 
+          url: articleId,
+        })
+        const originalURLRef = doc(db, 'hashedToOriginalURLs', articleId);
+        await setDoc(originalURLRef, {
+          originalURL: article.url,
+        })
+        setSavedArticles((prevSavedArticles) => [
+          ...prevSavedArticles,
+          { hashedURL: articleId, originalURL: article.url },
+        ])
+      }
+    } catch (error) {
+      console.error("Error saving article:", error)
+    }
+  }
+
   return (
     <div className="article-section">
       <div className="article-container">
         {(articles.length > 0 ? articles : newsData).map(article => (
-          <div key={article.url} className="article">
-            <h2>{article.title}</h2>
-            <h4>{article.author}</h4>
-            <img src={article.urlToImage} alt={article.title} />
-            <p className="description">{formatDescription(article.content)}</p>
-            <div className="info">
-              <p>{new Date(article.publishedAt).toLocaleDateString()}</p>
-              <p>{article.source.name}</p>
+            <div key={article.url} className="article">
+              <h2>{article.title}</h2>
+              <div className="info">
+                <h4>{article.author}</h4>
+                {currentUser && (
+                  <img
+                    src={savedArticles.some((savedArticle) => savedArticle.originalURL === article.url)
+                            ? StarIcon
+                            : EmptyStarIcon
+                        }
+                    alt="Star"
+                    onClick={() => toggleSaveArticles(article)}
+                    className="star-icon"
+                  />
+                )}
+              </div>
+              <img src={article.urlToImage} alt={article.title} />
+              <p className="description">{formatDescription(article.content)}</p>
+              <div className="info">
+                <p>{new Date(article.publishedAt).toLocaleDateString()}</p>
+                <p>{article.source.name}</p>
+              </div>
+              <a target="_blank" href={article.url}>
+                Read more
+              </a>
             </div>
-            <a target="_blank" href={article.url}>Read more</a>
-          </div>
-        ))}
+          ))}
         {loading && <div className="loading-indicator">Loading...</div>}
       </div>
     </div>
-  );
+  )
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import SignUp from './components/Pages/SignUpPage';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 import {AuthProvider} from "./contexts/AuthContexts"
@@ -10,13 +9,13 @@ import {AuthProvider} from "./contexts/AuthContexts"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AuthProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
